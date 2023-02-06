f9413ce8d8d9a93f8195077bc7b1e1cab0ba1163
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-06 11:55:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,71 @@
+export const item = {
+    "id": "761012e1-cf83-40ea-bb79-c8267eb86791",
+    "protect_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png",
+    "protect_title": "Station participant picture various let. Name child billion last difference none. Address particularly read choice trouble for management.",
+    "protect_main_title": "Loss some need tough enter baby than. Explain doctor thing degree wind nice design.",
+    "protect_box_title": "Like whose cover relationship surface let. Stock he attorney discuss.",
+    "about_protect": "Environmental part key know. College local home fund own strong. Region model high.",
+    "protect_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png",
+    "cred_profile_list": [
+        {
+            "id": "81c199c3-6605-4769-a6e6-b4fcdb767034",
+            "cred_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png",
+            "cred_title": "Inside international all hear special training friend. Son join smile decade.",
+            "cred_main_title": "That dinner such enjoy day. Increase source finally dark. Five option assume follow bag data.",
+            "content_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png",
+            "last_paid": "station",
+            "price": 8669,
+            "content_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png",
+            "view_all": "truth",
+            "transactions": "fall"
+        },
+        {
+            "id": "e6e5bbf2-7cf0-40de-b34c-28bed7aa48a7",
+            "cred_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png",
+            "cred_title": "Second anyone election have beyond situation. Remember feeling produce television. Pick team them necessary.",
+            "cred_main_title": "Southern thought perhaps game owner get. Reality color board ready vote main. Soon southern cut poor.",
+            "content_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png",
+            "last_paid": "particular",
+            "price": 8239,
+            "content_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png",
+            "view_all": "here",
+            "transactions": "effect"
+        },
+        {
+            "id": "d2dbcb4e-350c-4a47-b173-b124a6204b37",
+            "cred_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png",
+            "cred_title": "Oil career quite compare officer see write. Produce marriage themselves industry.",
+            "cred_main_title": "Mr she hit option response sense. Probably study close church traditional each.",
+            "content_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png",
+            "last_paid": "house",
+            "price": 1682,
+            "content_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png",
+            "view_all": "second",
+            "transactions": "beyond"
+        },
+        {
+            "id": "eea53b89-9136-4062-a047-6413842f7772",
+            "cred_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png",
+            "cred_title": "Training news minute least stand daughter above. Mrs example million walk well paper.",
+            "cred_main_title": "Fall everybody put appear institution mission wonder. Shake side already.",
+            "content_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png",
+            "last_paid": "step",
+            "price": 7959,
+            "content_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png",
+            "view_all": "energy",
+            "transactions": "try"
+        },
+        {
+            "id": "ab94ed6a-1d4d-425b-951c-d582e6d48dd8",
+            "cred_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png",
+            "cred_title": "Guy true thus gun seven. Person will newspaper pull investment own party. Among anyone way put dark skin song however.",
+            "cred_main_title": "Morning floor address director. Three staff reality old. Anything message story avoid mean join effect.",
+            "content_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png",
+            "last_paid": "charge",
+            "price": 9431,
+            "content_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png",
+            "view_all": "several",
+            "transactions": "far"
+        }
+    ]
+}
